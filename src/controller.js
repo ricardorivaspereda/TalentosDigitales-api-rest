af28ef8570d74ad4b11d5adc4e8757adad36908c
ricardorivaspereda <ricardorivaspereda@gmail.com>
2023-10-25 21:06:42 -0300

    correcciones en controller.js por errores encontrados

--- src/controller.js
+++ src/controller.js
@@ -9,13 +9,13 @@ class LibroController{
 
 
     async getOne(req,res) {
-        const [result] = await pool.query(`SELECT FROM Libros WHERE id=(?)`,[libro.id]);
+        const libroId = req.params.id;
+        const [result] = await pool.query(`SELECT * FROM libros WHERE id=(?)`,[libro.id]);
         res.json(result);
     }
 
 
 
-    /*  no borro porque tome el ejemplo de personas del video y me sirve para comprender 
 
     async add(req,res){
         const libro = req.body;
@@ -23,17 +23,7 @@ class LibroController{
         res.json({"Id insertado": result.insertId});
     }
 
-    async delete(req,res){
-        const libro = req.body;
-        const [result] = await pool.query(`DELETE FROM Libros WHERE id=(?)`,[libro.id]);
-        res.json({"Registros eliminados": result.affectedRows});
-    }
-
-    async update(req,res){
-        const libro = req.body;
-        const [result] = await pool.query(`UPDATE Libros SET nombre=(?), autor=(?), categoria=(?), año-publicacion=(?), ISBN=(?) WHERE id=(?)`,[libro.nombre,libro.autor,libro.categoria,libro.año-publicacion,libro.ISBN, libro.id]);
-        res.json({"Registros actualizados": result.changedRows});
-    }*/
+   
 
 }
 
