f700913c691cc08473e6b1cc770c746fc7490e18
ricardorivaspereda <ricardorivaspereda@gmail.com>
2023-10-30 23:57:42 -0300

    se define ruta al metodo "geetOne" de la clase LibroController

--- src/routes.js
+++ src/routes.js
@@ -4,6 +4,7 @@ import { libro } from "./controller.js";
 export const router = Router();
 
 router.get('/libros', libro.getAll);
+router.get('/libros/:id', libro.getOne);
 router.post('/libros', libro.add);
 router.delete('/libros/:id', libro.delete);
 router.put('/libros/:id', libro.update);
